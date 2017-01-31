4cc8bf932b8795eef1ebb81e7925c9401bf953ca
Chris MacIntosh <sobe911@icloud.com>
2017-01-31 00:57:24 -0800

    Add resources folder, move images, fix references.

--- autocropper/Index.Designer.cs
+++ autocropper/Index.Designer.cs
@@ -161,7 +161,6 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Honeydew;
-            this.BackgroundImage = global::autocropper.Properties.Resources.road;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(284, 261);
             this.Controls.Add(this.label4);
